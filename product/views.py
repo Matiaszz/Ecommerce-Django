eacbf2b1437110e917d36a719161b6077fccf4b0
Allan Giovanni Matias <alangiovanepaes@gmail.com>
2024-11-01 13:58:39 -0300

    learing about Sessions and stating to do the features of AddToCart method

--- product/views.py
+++ product/views.py
@@ -1,8 +1,10 @@
 # pylint: disable=all
+from django.shortcuts import redirect, reverse, get_object_or_404  # type: ignore
 from django.views.generic.detail import DetailView
 from django.views.generic.list import ListView
 from django.views import View
 from django.http import HttpResponse
+from django.contrib import messages
 from . import models
 
 
@@ -22,7 +24,29 @@ class ProductDetail(DetailView):
 
 class AddToCart(View):
     def get(self, *args, **kwargs):
-        return HttpResponse('AddToCart')
+        referer = self.request.META.get(
+            'HTTP_REFERER', reverse('product:list'))
+        id_variation = self.request.GET.get('vid')
+
+        if not id_variation:
+            messages.error(self.request, 'Produto não encontrado.')
+            return redirect(referer)
+
+        variation = get_object_or_404(models.Variation, id=id_variation)
+
+        if not self.request.session.get('cart'):
+            self.request.session['cart'] = {}
+            self.request.session.save()
+
+        cart = self.request.session['cart']
+        if id_variation in cart:
+            # TODO: Exists variation in cart
+            pass
+        else:
+            # TODO: Variation dont existis in cart
+
+            pass
+        return HttpResponse(f'{variation.product} {variation.name}')
 
 
 class RemoveFromCart(View):
